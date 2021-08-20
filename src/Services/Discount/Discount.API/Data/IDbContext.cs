494deae6f8188d0b6628a3d149020b3e19ca583a
aabdelhaleemm <abdfre7at@gmail.com>
2021-08-20 14:55:48 +0300

    add discount.api

---
+++ src/Services/Discount/Discount.API/Data/IDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace Discount.API.Data
+{
+    public interface IDbContext
+    {
+        public IDbConnection DbConnection { get ;  }
+    }
+}
