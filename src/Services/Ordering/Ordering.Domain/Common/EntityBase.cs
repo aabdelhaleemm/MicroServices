1f5b157e2c5f5ccadcd3533b208abe769f6d3179
aabdelhaleemm <abdfre7at@gmail.com>
2021-08-26 19:05:12 +0300

    finish ordering service

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedAt { get; set; } 
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Seed database associated with context {nameof(OrderContext)}");
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "abd", FirstName = "Abdelhaleem", LastName = "Alfreihat",
+                    EmailAddress = "abdfre7at@gmail.com", AddressLine = "jordan", Country = "jordan", TotalPrice = 350
+                }
+            };
+        }
+    }
+}
